f0a3ab300903ca3af89e31f070f615572a5ff089
zachuri <punsalang.zachary@gmail.com>
2022-02-22 03:40:53 -0800

    Setup dependencies/ Create Front End/ Pass Props/ Mangae State

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{/* Today's Task */}
+			<View style={styles.tasksWrapper}>
+				<Text style={styles.sectionTitle}>Today's Task</Text>
+
+				<View style={styles.items}>
+					{/* This is where the tasks will go! */}
+					<Task text={'Task 1'} />
+					<Task text={'Task 2'} />
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#E8EAED',
+	},
+
+	tasksWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+
+	sectionTitle: { fontSize: 24, fontWeight: 'bold' },
+	items: { marginTop: 30 },
 });
